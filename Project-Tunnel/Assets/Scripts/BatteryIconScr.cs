e397a75655fe1926f8edd5b49da177b4930149f5
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-02-24 10:39:55 -0700

    Added blinking rec light and additions to battery script

--- Project-Tunnel/Assets/Scripts/BatteryIconScr.cs
+++ Project-Tunnel/Assets/Scripts/BatteryIconScr.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BatteryIconScr : MonoBehaviour
 {
 
     public float secondsCount = 0;
+
+    public float secondsDown = 0;
 
     public GameObject batteryFull;
     public GameObject batteryFiveSix;
@@ -17,6 +20,13 @@ public class BatteryIconScr : MonoBehaviour
 
     public GameObject recObject;
     public GameObject lightObject;
+    public Image image;
+    public GameObject recDotObject;
+
+    public bool hasBattery = true;
+    public bool isOut = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +36,22 @@ public class BatteryIconScr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        BatteryCheck();
+        if (hasBattery)
+        {
+            BatteryCheck();
+        }
+
+        if (isOut)
+        {
+            BatteryReset();
+        }
+
     }
 
 
     void BatteryCheck()
     {
+        secondsDown = 0;
         secondsCount += Time.deltaTime;
 
         if (secondsCount > 60)
@@ -58,11 +78,23 @@ public class BatteryIconScr : MonoBehaviour
                             {
                                 batteryOneSix.SetActive(false);
                                 batteryZeroSix.SetActive(true);
-                                if (secondsCount >= 380)
+
+                                recObject.SetActive(false);
+                                recDotObject.SetActive(false);
+                                StopAllCoroutines();
+
+                                StartBlinking();
+                                if (secondsCount >= 390)
                                 {
+                                    // StopBlinking();
+                                    StopAllCoroutines();
+
                                     batteryZeroSix.SetActive(false);
                                     recObject.SetActive(false);
                                     lightObject.SetActive(false);
+                                    recDotObject.SetActive(false);
+                                    hasBattery = false;
+                                    isOut = true;
 
                                 }
                             }
@@ -75,8 +107,55 @@ public class BatteryIconScr : MonoBehaviour
         }
     }
 
-    void BlinkingIcon()
-    {
 
+
+
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (image.color.a.ToString())
+            {
+                case "0":
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+                    //Play sound
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+                    //Play sound
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+            }
+        }
     }
+
+    void StartBlinking()
+    {
+        StopAllCoroutines();
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopAllCoroutines();
+    }
+
+    void BatteryReset()
+    {
+        secondsDown += Time.deltaTime;
+        if (secondsDown > 7)
+        {
+            secondsCount = 0;
+            isOut = false;
+            hasBattery = true;
+            batteryFull.SetActive(true);
+            recObject.SetActive(true);
+            lightObject.SetActive(true);
+            recDotObject.SetActive(true);
+        }
+    }
+
 }
+
