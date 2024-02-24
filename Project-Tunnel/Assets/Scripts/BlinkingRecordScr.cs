f136296a21da08e19ad73e8129aa2c94145f8866
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-02-24 12:57:44 -0700

    Added different battery icon aspect

--- Project-Tunnel/Assets/Scripts/BlinkingRecordScr.cs
+++ Project-Tunnel/Assets/Scripts/BlinkingRecordScr.cs
@@ -7,6 +7,9 @@ public class BlinkingRecordScr : MonoBehaviour
 {
     public Image image;
 
+    public Image recordIcon;
+    public Image recordIconNoDot;
+
     void Start()
     {
         // image = GetComponent<Image>();
@@ -22,11 +25,25 @@ public class BlinkingRecordScr : MonoBehaviour
             {
                 case "0":
                     image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+
+                    // recordIcon.SetActive(true);
+                    // recordIconNoDot.SetActive(false);
+
+                    // recordIcon.color = new Color(recordIcon.color.r, recordIcon.color.g, recordIcon.color.b, 1);
+                    // recordIconNoDot.color = new Color(recordIconNoDot.color.r, recordIconNoDot.color.g, recordIconNoDot.color.b, 0);
+
                     //Play sound
                     yield return new WaitForSeconds(0.5f);
                     break;
                 case "1":
                     image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+
+                    // recordIcon.SetActive(false);
+                    // recordIconNoDot.SetActive(true);
+
+                    // recordIcon.color = new Color(recordIcon.color.r, recordIcon.color.g, recordIcon.color.b, 0);
+                    // recordIconNoDot.color = new Color(recordIconNoDot.color.r, recordIconNoDot.color.g, recordIconNoDot.color.b, 1);
+
                     //Play sound
                     yield return new WaitForSeconds(0.5f);
                     break;
