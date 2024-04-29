bcf635d90169a64515eaf91fdd0477afbbd1cb7d
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-04-29 13:14:28 -0600

    Added rubble in elevator shaft

--- Project-Tunnel/Assets/Scripts/Old Scripts/PlayerMovementScr.cs
+++ Project-Tunnel/Assets/Scripts/Old Scripts/PlayerMovementScr.cs
@@ -72,10 +72,16 @@ public class PlayerMovementScr : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        // if (isGrounded && velocity.y < 0)
+        // {
+        //     velocity.y = -2f;
+        // }
+
+        if (velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -9.81f;
         }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
