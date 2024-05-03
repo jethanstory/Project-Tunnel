63f325e2a7cb05e1832ba760d2edc8fa03101b45
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-05-03 00:01:06 -0600

    Added flashback test aspect

---
+++ Project-Tunnel/Assets/Scripts/FlashbackTestScr.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashbackTestScr : MonoBehaviour
+{
+    public GameObject ev;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if (other.gameObject.tag == "FlashbackTest") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Debug.Log("HIT");
+            ev.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "FlashbackTest") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            Cursor.lockState = CursorLockMode.None;
+            ev.SetActive(false);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
