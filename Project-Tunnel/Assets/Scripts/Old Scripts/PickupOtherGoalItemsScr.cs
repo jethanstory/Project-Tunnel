301549f2953a0b3851043502e9e03708a03a4da6
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-05-05 11:43:40 -0600

    Added hints for the goal objects for both canvases

--- Project-Tunnel/Assets/Scripts/Old Scripts/PickupOtherGoalItemsScr.cs
+++ Project-Tunnel/Assets/Scripts/Old Scripts/PickupOtherGoalItemsScr.cs
@@ -10,6 +10,26 @@ public class PickupOtherGoalItemsScr : MonoBehaviour
 
     public GameObject keySound;
 
+    public bool rustyKnifePickup;
+    public bool crateCrowbarPickup;
+    public bool allenKeyPickup;
+
+    public bool oldCam;
+
+    public GameObject rustyKnifePickupNotification;
+
+    public GameObject crateCrowbarPickupNotification;
+
+    public GameObject allenKeyPickupNotification;
+
+    public GameObject rustyKnifePickupNotificationOldCam;
+
+    public GameObject crateCrowbarPickupNotificationOldCam;
+
+    public GameObject allenKeyPickupNotificationOldCam;
+
+    public float secondsCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,33 +39,91 @@ public class PickupOtherGoalItemsScr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (rustyKnifePickup)
+        {
+            secondsCount += Time.deltaTime;
+            rustyKnifePickupNotification.SetActive(true);
 
+            if (secondsCount > 2) 
+            {
+                rustyKnifePickupNotification.SetActive(false);
+                //endTime = true;
+                //secondsCount = 100;
+                // textGoal.SetActive(true);
+                // if (secondsCount > 10)
+                // {
+                //     textGoal.SetActive(false);
+                // }
+            }
+        }
+
+        if (crateCrowbarPickup)
+        {
+            secondsCount += Time.deltaTime;
+            crateCrowbarPickupNotification.SetActive(true);
+
+            if (secondsCount > 2) 
+            {
+                crateCrowbarPickupNotification.SetActive(false);
+                //endTime = true;
+                //secondsCount = 100;
+                // textGoal.SetActive(true);
+                // if (secondsCount > 10)
+                // {
+                //     textGoal.SetActive(false);
+                // }
+            }
+        }
+        if (allenKeyPickup)
+        {
+            secondsCount += Time.deltaTime;
+            allenKeyPickupNotification.SetActive(true);
+
+            if (secondsCount > 2) 
+            {
+                allenKeyPickupNotification.SetActive(false);
+                //endTime = true;
+                //secondsCount = 100;
+                // textGoal.SetActive(true);
+                // if (secondsCount > 10)
+                // {
+                //     textGoal.SetActive(false);
+                // }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
-        if(other.gameObject.tag == "AllenKey") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if(other.gameObject.tag == "AllenKeyPickup") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
+            secondsCount = 0;
             canpickup = true;  //set the pick up bool to true
             ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
             Destroy(ObjectIwantToDestroy);
+            allenKeyPickup = true;
             keySound.SetActive(false);
             keySound.SetActive(true);
+            rustyKnifePickupNotification.SetActive(true);
         }
         
-        if(other.gameObject.tag == "RustyKnife") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if(other.gameObject.tag == "RustyKnifePickup") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
+            secondsCount = 0;
             canpickup = true;  //set the pick up bool to true
             ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
             Destroy(ObjectIwantToDestroy);
+            rustyKnifePickup = true;
             keySound.SetActive(false);
             keySound.SetActive(true);
         }
-        if(other.gameObject.tag == "RustyKnife") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if(other.gameObject.tag == "CrateCrowbarPickup") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
+            secondsCount = 0;
             canpickup = true;  //set the pick up bool to true
             ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
             Destroy(ObjectIwantToDestroy);
+            crateCrowbarPickup = true;
             keySound.SetActive(false);
             keySound.SetActive(true);
         }
