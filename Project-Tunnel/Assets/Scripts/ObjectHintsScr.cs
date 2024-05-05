de15fa14c22da7723f95bf5039ac477a32d7099b
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-05-05 16:20:30 -0600

    Added animations for crate and crowbar

--- Project-Tunnel/Assets/Scripts/ObjectHintsScr.cs
+++ Project-Tunnel/Assets/Scripts/ObjectHintsScr.cs
@@ -9,17 +9,52 @@ public class ObjectHintsScr : MonoBehaviour
     public GameObject crateCrowbar;
     public GameObject allenKey;
 
+    bool inCrateArea;
+
+    public bool crowbarCheck = true;
+
+    public GameObject animatedCrowbar;
+
+    
+
+    public AnimationClip pry;
+    public AnimationClip open;
+    public Animation animCrow;
+    public Animation animLid;
+
+    public GameObject animatedLid;
+    public GameObject origCrate;
+    public GameObject animatedCrate;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //anim = GetComponent<Animation>();
+        animCrow = animatedCrowbar.GetComponent<Animation>();
+        animLid = animatedLid.GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inCrateArea)
+        {
+            // Debug.Log("This shit works");
+            if (crowbarCheck)
+            {
+                origCrate.SetActive(false);
+                animatedCrate.SetActive(true);
+                animatedCrowbar.SetActive(true);
+                animCrow.clip = pry;
+                animCrow.Play();
+                animLid.clip = open;
+                animLid.Play();
+
+                // animatedCrowbar.SetActive(false);
+
+            }
+        }
     }
 
 
@@ -33,6 +68,8 @@ public class ObjectHintsScr : MonoBehaviour
         if(other.gameObject.tag == "CrateCrowbarHint") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             crateCrowbar.SetActive(true);
+            inCrateArea = true;
+
         }
         if(other.gameObject.tag == "allenKeyHint") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
