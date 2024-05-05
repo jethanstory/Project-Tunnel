8f9d35e6735ef075f114404beb3941328a1dbf3d
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-05-05 10:23:31 -0600

    Added some knife and key assets and another script for the goal objects

---
+++ Project-Tunnel/Assets/Scripts/Old Scripts/PickupOtherGoalItemsScr.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupOtherGoalItemsScr : MonoBehaviour
+{
+
+    bool canpickup; //a bool to see if you can or cant pick up the item
+    GameObject ObjectIwantToDestroy; // the gameobject onwhich you collided with
+
+    public GameObject keySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "AllenKey") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            canpickup = true;  //set the pick up bool to true
+            ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
+            Destroy(ObjectIwantToDestroy);
+            keySound.SetActive(false);
+            keySound.SetActive(true);
+        }
+        
+        if(other.gameObject.tag == "SecondKey") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            canpickup = true;  //set the pick up bool to true
+            ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
+            Destroy(ObjectIwantToDestroy);
+            keySound.SetActive(false);
+            keySound.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canpickup = false; //when you leave the collider set the canpickup bool to false
+     
+    }
+}
