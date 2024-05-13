d4fc1aa2896c9885a4df20f34fc9f624aa3c211f
jethanstory <32407746+jethanstory@users.noreply.github.com>
2024-05-13 00:24:58 -0600

    Added experimental sanity aspect

---
+++ Project-Tunnel/Assets/Scripts/TimeInDarkScr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeInDarkScr : MonoBehaviour
+{
+
+    public float totalTime;
+    public GameObject scareObject;
+    public Transform spawnPoint;
+    public bool canSpawn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+
+        if (totalTime > 30 && totalTime < 30.1)
+        {
+            canSpawn = true;
+        }
+        SpawnAspect();
+    }
+
+    void SpawnAspect()
+    {
+        if (canSpawn)
+        {
+            GameObject scareInstance = Instantiate(scareObject, spawnPoint.position, spawnPoint.rotation);
+            canSpawn = false;
+        }
+    }
+}
